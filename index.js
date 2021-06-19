3da91cbd484d93888dd199bfb7176ca57bae18d6
Amirerfan Rafati <aerfanr@mailo.com>
2021-06-19 21:11:30 +0430

    no comment

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const linkHtml = '<a href="https://nc.aerfanr.ir/apps/calendar/p/PtjXxopxaK3EMtL9"> https://nc.aerfanr.ir/apps/calendar/p/PtjXxopxaK3EMtL9</a>'
-
-document.getElementById("cal-url").innerHTML = "Currently not available";
-
+document.getElementById('cal-url').style.display = 'none'
 fetch('https://status.aerfanr.ir')
 	.then((response) => {
 		return response.json()
 	})
 	.then((data) => {
 		if (data.status === 'success') {
-			document.getElementById('cal-url').innerHTML = linkHtml;
+			document.getElementById('cal-url').style.display = 'inline'
+			document.getElementById('cal-not-available').style.display = 'none'
 		}
 	})
